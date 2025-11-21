60cb333d731955683f7431e7cc3628efc532211b
pratap360 <pratapparui0360@gmail.com>
2025-11-21 17:42:56 +0530

    final commit for nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgbCollapseModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'qfinance';
-
-  
+  isMenuCollapsed = true;
 }
